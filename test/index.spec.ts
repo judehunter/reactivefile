a079bea8c9889af9ea769df0c04e2b5c46ecc5ec
judehunter <matisowagm@gmail.com>
2020-07-24 11:21:34 +0200

    More tests, npm package setup

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,12 +1,12 @@
-import rf from '../src';
+import ReactiveFile from '../src';
 import * as fs from 'fs';
 import * as yml from 'js-yaml';
 import * as xml from 'fast-xml-parser';
 import * as toml from '@iarna/toml';
 
 test('load JSON file', async () => {
-  expect(rf.loadSync(__dirname + '/fixtures/jsonfile.json')).toBeDefined()
-  expect(await rf.load(__dirname + '/fixtures/jsonfile.json')).toBeDefined()
+  expect(ReactiveFile.loadSync(__dirname + '/fixtures/jsonfile.json')).toBeDefined()
+  expect(await ReactiveFile.load(__dirname + '/fixtures/jsonfile.json')).toBeDefined()
 })
 
 const testObject = {
@@ -22,9 +22,9 @@ const testObject = {
 test('JSON ReactiveFile', async () => {
   const from = __dirname + '/fixtures/jsonfile.json';
   const to = __dirname + '/temp/jsonfile.json';
-  const data = await rf.load(from, {saveTo: to, asyncSave: false});
-  data.$.abc = 'ghi';
-  data.$.deep.nested.thing = 456;
+  const data = await ReactiveFile.load(from, {saveTo: to, asyncSave: false});
+  data.val.abc = 'ghi';
+  data.val.deep.nested.thing = 456;
   
   expect(JSON.parse((await fs.promises.readFile(to)).toString())).toEqual(testObject);
 })
@@ -32,7 +32,7 @@ test('JSON ReactiveFile', async () => {
 test('YAML ReactiveFile', async () => {
   const from = __dirname + '/fixtures/yamlfile.yml';
   const to = __dirname + '/temp/yamlfile.yml';
-  const data = await rf.load(from, {saveTo: to, asyncSave: false});
+  const data = await ReactiveFile.load(from, {saveTo: to, asyncSave: false});
   data.val.abc = 'ghi';
   data.val.deep.nested.thing = 456;
 
@@ -42,7 +42,7 @@ test('YAML ReactiveFile', async () => {
 test('TOML ReactiveFile', async () => {
   const from = __dirname + '/fixtures/tomlfile.toml';
   const to = __dirname + '/temp/tomlfile.toml';
-  const data = await rf.load(from, {saveTo: to, asyncSave: false});
+  const data = await ReactiveFile.load(from, {saveTo: to, asyncSave: false});
   data.val.abc = 'ghi';
   data.val.deep.nested.thing = 456;
 
@@ -52,7 +52,7 @@ test('TOML ReactiveFile', async () => {
 test('XML ReactiveFile', async () => {
   const from = __dirname + '/fixtures/xmlfile.xml';
   const to = __dirname + '/temp/xmlfile.xml';
-  const data = await rf.load(from, {saveTo: to, asyncSave: false});
+  const data = await ReactiveFile.load(from, {saveTo: to, asyncSave: false});
   data.val.abc = 'ghi';
   data.val.deep.nested.thing = 456;
 
@@ -62,27 +62,68 @@ test('XML ReactiveFile', async () => {
 test('non-deep reactiveness', async () => {
   const from = __dirname + '/fixtures/jsonfile.json';
   const to = __dirname + '/temp/jsonfile.json';
-  const data = await rf.load(from, {saveTo: to, deep: false, asyncSave: false});
+  const data = await ReactiveFile.load(from, {saveTo: to, deep: false, asyncSave: false});
   data.val.abc = 'ghi';
   data.val.deep.nested.thing = 456;
 
   expect(JSON.parse((await fs.promises.readFile(to)).toString())).toEqual(
-    JSON.parse('{"abc": "ghi", "deep": {"nested": {"thing": 123}}, "array": [1, 4, 9]}')
+    {...testObject, deep: {nested: {thing: 123}}}
   );
 })
 
 test('add key and .react()', async () => {
   const from = __dirname + '/fixtures/jsonfile.json';
   const to = __dirname + '/temp/jsonfile.json';
-  const data = await rf.load(from, {saveTo: to, deep: false, asyncSave: false});
+  const data = await ReactiveFile.load(from, {saveTo: to, asyncSave: false});
   data.val.abc = 'ghi';
   data.val.deep.nested.thing = 456;
 
-  expect(JSON.parse((await fs.promises.readFile(to)).toString())).toEqual(
-    JSON.parse('{"abc": "ghi", "deep": {"nested": {"thing": 123}}, "array": [1, 4, 9]}')
-  );
+  expect(JSON.parse((await fs.promises.readFile(to)).toString())).toEqual(testObject);
+
+  data.val.newkey = 123;
+  data.react();
+
+  expect(JSON.parse((await fs.promises.readFile(to)).toString())).toEqual({...testObject, newkey: 123});
 })
 
-// afterAll(async () => {
-//   fs.rmdirSync(__dirname + '/temp', {recursive: true});
-// });
+test('.assignType()', async () => {
+  {
+    const from = __dirname + '/fixtures/customjsonfile.custom';
+    const to = __dirname + '/temp/customjsonfile.custom';
+
+    ReactiveFile.assignType('mytype', 'json');
+
+    const data = await ReactiveFile.load(from, {saveTo: to, asyncSave: false, type: 'mytype'});
+    data.val.abc = 'ghi';
+    data.val.deep.nested.thing = 456;
+
+    expect(JSON.parse((await fs.promises.readFile(to)).toString())).toEqual(testObject);
+  }
+  {
+    const from = __dirname + '/fixtures/customjsonfile.custom';
+    const to = __dirname + '/temp/customjsonfile.custom';
+
+    ReactiveFile.assignType('custom', 'json');
+
+    const data = await ReactiveFile.load(from, {saveTo: to, asyncSave: false});
+    data.val.abc = 'ghi';
+    data.val.deep.nested.thing = 456;
+
+    expect(JSON.parse((await fs.promises.readFile(to)).toString())).toEqual(testObject);
+  }
+})
+
+test('$ alias', async () => {
+  const from = __dirname + '/fixtures/jsonfile.json';
+  const to = __dirname + '/temp/jsonfile.json';
+  const data = await ReactiveFile.load(from, {saveTo: to, asyncSave: false});
+  data.$.abc = 'ghi';
+  data.$.deep.nested.thing = 456;
+
+  expect(JSON.parse((await fs.promises.readFile(to)).toString())).toEqual(testObject);
+})
+
+// comment this out for debugging purposes
+afterAll(async () => {
+  fs.rmdirSync(__dirname + '/temp', {recursive: true});
+});
